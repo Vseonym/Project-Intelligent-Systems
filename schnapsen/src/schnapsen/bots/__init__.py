4fa82250282ae6ecd6b38b5b244e14678023be34
Vseonym <george.konditerov@gmail.com>
2024-01-18 17:17:23 +0100

    new_ml_bot

--- schnapsen/src/schnapsen/bots/__init__.py
+++ schnapsen/src/schnapsen/bots/__init__.py
@@ -7,5 +7,6 @@ from .rdeep import RdeepBot
 from .ml_bot import MLDataBot, MLPlayingBot, train_ML_model
 from .gui.guibot import SchnapsenServer
 from .minimax import MiniMaxBot
+from .nn_bot import NNDataBot, NNPlayingBot, train_NN_model
 
-__all__ = ["RandBot", "AlphaBetaBot", "RdeepBot", "MLDataBot", "MLPlayingBot", "train_ML_model", "SchnapsenServer", "MiniMaxBot"]
+__all__ = ["RandBot", "AlphaBetaBot", "RdeepBot", "MLDataBot", "MLPlayingBot", "train_ML_model", "SchnapsenServer", "MiniMaxBot", "NNDataBot", "NNPlayingBot", "train_NN_model"]
